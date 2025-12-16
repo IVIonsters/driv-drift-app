54de9ae9caad112f7a0c03d0b817ae8e2d600323
Zachary Polof <zacharypolof@gmail.com>
2025-12-15 21:10:43 -0600

    Add routing with react-router-dom and remove unused App component

--- driv-frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import DevelopmentPage from "./pages/developmentPage.jsx";
-
-function App() {
-  return <DevelopmentPage />;
-}
-
-export default App;
--- driv-frontend/src/main.jsx
+++ driv-frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+//Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Styles
+import "./index.css";
+
+// Pages
+import DevelopmentPage from "./pages/developmentPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DevelopmentPage />,
+  },
+  {
+    path: "/homepage",
+    element: <HomePage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
