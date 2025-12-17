1c17eb3c5c27712e4258d31d33302b5712523038
Zachary Polof <zacharypolof@gmail.com>
2025-12-16 22:25:15 -0600

    feat: update Button component styles, and clean up HomePage

--- driv-frontend/src/pages/HomePage.jsx
+++ driv-frontend/src/pages/HomePage.jsx
@@ -1,14 +1,3 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
-
 import { Link } from "react-router-dom";
 
 import Navbar from "@/components/NavBar";
