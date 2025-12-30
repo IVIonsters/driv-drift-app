08addc6867666593ee573fc79908c714eb98541d
Zachary Polof <zacharypolof@gmail.com>
2025-12-29 21:46:27 -0600

    feat: implement LoginForm component and integrate with LoginPage

--- driv-frontend/src/features/loginpage/pages/LoginPage.jsx
+++ driv-frontend/src/features/loginpage/pages/LoginPage.jsx
@@ -1,9 +1,12 @@
+import LoginForm from "@/components/login-form";
+
 function LoginPage() {
   return (
-    <>
-      <p>Hello Login Page</p>
-      <p>Testing Login</p>
-    </>
+    <div className="min-h-screen text-center flex items-center justify-center px-4">
+      <div className="w-full max-w-md">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
 
